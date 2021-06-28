eff3a8ddc5b200be820dd9712b29a105dfac71d9
SANKIT KUNDU <77527865+san234751@users.noreply.github.com>
2021-06-28 13:34:49 +0530

    updated connection

--- Api/Connection.js
+++ Api/Connection.js
@@ -9,25 +9,3 @@ const connectDB = async () => {
   console.log("db connected..!");
 };
 module.exports = connectDB;
-const mongoose = require("mongoose");
-const URI =
-  "mongodb+srv://sankit:fG4MxbFEyAWhAEG@website.vn7l4.mongodb.net/test";
-const connectDB = async () => {
-  await mongoose.connect(URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
-  console.log("db connected..!");
-};
-module.exports = connectDB;
-const mongoose = require("mongoose");
-const URI =
-  "mongodb+srv://sankit:fG4MxbFEyAWhAEG@website.vn7l4.mongodb.net/test";
-const connectDB = async () => {
-  await mongoose.connect(URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
-  console.log("db connected..!");
-};
-module.exports = connectDB;
