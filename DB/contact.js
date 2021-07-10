bb16c832453d2cdc9ab5c1157fa5e5d7349aec90
SANKIT KUNDU <77527865+san234751@users.noreply.github.com>
2021-07-10 10:46:25 +0530

    Update contact.js

--- DB/contact.js
+++ DB/contact.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const PostSchema = mongoose.Schema({
   email: { type: String, require: true },
-  message: { type: String, require: true },
+  message: { type: String, require: true }
 });
 module.exports = mongoose.model("contact", PostSchema);
