a63a19a542b39c7f64c4a4ea6268196fb943265a
SANKIT KUNDU <77527865+san234751@users.noreply.github.com>
2021-07-10 10:43:43 +0530

    Update Connection.js

--- Api/Connection.js
+++ Api/Connection.js
@@ -4,7 +4,7 @@ const URI =
 const connectDB = async () => {
   await mongoose.connect(URI, {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
+    useNewUrlParser: true
   });
   console.log("db connected..!");
 };
