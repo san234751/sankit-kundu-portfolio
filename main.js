8c65521689dfee3b11b0233ef55c33d9329d3450
Sankit Kundu <sankitkundu96@gmail.com>
2021-12-24 22:47:51 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+sr.reveal('.transform1',{}); 
+sr.reveal('.home_image',{delay: 400}); 
+sr.reveal('.social_icon',{ interval: 200}); 
+sr.reveal('.subtitle',{}); 
+sr.reveal('.text',{}); 
+sr.reveal('.data',{interval: 200}); 
+sr.reveal('.img1',{delay: 600});
+sr.reveal('.img',{interval: 200}); 
